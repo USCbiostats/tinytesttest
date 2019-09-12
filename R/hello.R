1ef6442d8a1d5f7aa93a4e6e244976d5b1e0a718
George G. Vega Yon <g.vegayon@gmail.com>
2019-09-12 10:00:57 -0700

    Adding Makevars to Rbuildignore and closing the cluster

--- R/hello.R
+++ R/hello.R
@@ -2,7 +2,7 @@
 #' @export
 #' @import parallel
 hello_parallel <- function() {
-  # on.exit(tryCatch(parallel::stopCluster(cl), error = function(e) e))
+  on.exit(tryCatch(parallel::stopCluster(cl), error = function(e) e))
   cl <- parallel::makeCluster(2)
   parallel::clusterEvalQ(cl, "hello")
 }
